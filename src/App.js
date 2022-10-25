c0aa67c2984f6ed0a0fd3c6bb59be603c6aad1e8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-25 17:50:09 +0300

    import navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
 import './App.scss';
 
 export default function App() {
   return (
     <div className='app'>
+      <Navbar />
       <Header />
       <About />
       <Work />

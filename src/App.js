96dffa7b30461903f9d22cf4ed2ea3fc2e5050da
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-25 17:31:37 +0300

    readd app.scss + import containers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-function App() {
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import './App.scss';
+
+export default function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
-
-export default App;

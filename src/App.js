8dc0b3443ce7b35a0f7ac3ee6a1aa8f00b13e8d5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-25 17:15:39 +0300

    format

--- src/App.js
+++ src/App.js
@@ -2,17 +2,12 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
       </header>

6b67c0eb82baa8dd2c64b95d3f3754187cca3d1b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-27 19:45:07 +0300

    fix imports, change token variable name

--- src/client.js
+++ src/client.js
@@ -1,14 +1,14 @@
-import { sanityClient } from '@sanity/client';
-import { ImageUrlBuilder } from '@sanity/image-url';
+import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
   projectId: process.env.REACT_APP_PROJECT_ID,
   dataset: process.env.REACT_APP_DATASET,
   apiVersion: process.env.REACT_APP_API_VERSION,
   useCdn: true,
-  token: process.env.REACT_APP_TOKEN,
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
-const builder = ImageUrlBuilder(client);
+const builder = imageUrlBuilder(client);
 
 export const urlFor = (source) => builder.image(source);

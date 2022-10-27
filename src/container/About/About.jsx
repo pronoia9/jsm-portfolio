9bd2950c1bebcb2d7fc2e1bcdcecc183655867f3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-27 19:10:15 +0300

    add init/temp profile items

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+
 import './About.scss';
 
+const abouts = [
+  { title: 'Web Development', description: 'I am a good web developer', imageUrl: '' },
+  { title: 'Web Design', description: 'I am a good web developer', imageUrl: '' },
+  { title: 'UI/UX', description: 'I am a good web developer', imageUrl: '' },
+  { title: 'Web Animations', description: 'I am a good web developer', imageUrl: '' },
+];
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <h2 className='head-text'>
+        I know that <span>good design</span>
+        <br /> means <span>good business</span>
+      </h2>
+
+      <div className='app__profiles'>
+        {abouts.map((about, i) => (
+          <motion.div
+            className='app__profile-item'
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            key={about.title + i}>
+            <img src={about.imageUrl} alt={about.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className='p-text' style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;

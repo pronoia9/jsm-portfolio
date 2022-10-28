682181fabc97ab19546fbd190d63400ac626a3fb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-28 16:37:05 +0300

    init work

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,7 +1,13 @@
+import { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
-  return <div>Work</div>;
+  return <div className=''></div>;
 };
 
-export default Work;
+export default AppWrap(MotionWrap(Work, 'app__works'), 'work', 'app__primarybg');

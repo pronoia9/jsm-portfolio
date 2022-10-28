862cd2ccc53fd97529529e5a79346a00416ce041
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-28 16:09:43 +0300

    finish social media

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,5 +1,20 @@
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+
 const SocialMedia = () => {
-  return <div>SocialMedia</div>;
+  return (
+    <div className='app__social'>
+      <div>
+        <BsTwitter />
+      </div>
+      <div>
+        <FaFacebookF />
+      </div>
+      <div>
+        <BsInstagram />
+      </div>
+    </div>
+  );
 };
 
 export default SocialMedia;

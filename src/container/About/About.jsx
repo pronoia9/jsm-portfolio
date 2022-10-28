88b216baf6439de01aa3403b899d2c7949fbf6da
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-28 16:29:06 +0300

    add app wrap to about

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import './About.scss';
+import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
+import './About.scss';
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
@@ -37,4 +38,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, 'about');

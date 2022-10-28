4fd1c67c8b82757c6de33cd20ee249e52ed8c62e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-28 16:17:46 +0300

    finish app wrap

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,7 +1,23 @@
-import { NavigationDots, SocialMedia } from "../components";
+import { NavigationDots, SocialMedia } from '../components';
 
-const AppWrap = () => {
-  return <div>AppWrap</div>;
-};
+const AppWrap = (Component, idName, classNames) =>
+  function HOC() {
+    return (
+      <div id={idName} className={`app_container ${classNames}`}>
+        <SocialMedia />
+
+        <div className='app__wrapper app__flex'>
+          <Component />
+
+          <div className='copyright'>
+            <p className='p-text'>@2022 Jay</p>
+            <p className='p-text'>All rights reserved</p>
+          </div>
+        </div>
+
+        <NavigationDots active={idName} />
+      </div>
+    );
+  };
 
 export default AppWrap;

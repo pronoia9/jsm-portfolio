a5d2a3e2e1c2a3cbf37dd12dbc6a41872251004d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-28 16:40:29 +0300

    add static/temp categories

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -6,8 +6,23 @@ import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Work.scss';
 
+//! TEMP
+const categories = ['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'];
+
 const Work = () => {
-  return <div className=''></div>;
+  const handleWorkFilter = () => { }
+  
+  return (
+    <>
+      <h2 className='head-text'>
+        My creative <span>portfolio</span> section
+      </h2>
+
+      <div className='app__work-filter'>
+        {categories.map((item, i) => (<div className='' key={i} onClick={() => handleWorkFilter(item)}></div>))}
+      </div>
+    </>
+  );
 };
 
 export default AppWrap(MotionWrap(Work, 'app__works'), 'work', 'app__primarybg');

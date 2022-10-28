2dd0b8d1c7272fc4b156075044da44fd3a13c7f3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-28 16:14:24 +0300

    finish navigation dots

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -1,5 +1,17 @@
-const NavigationDots = () => {
-  return <div>NavigationDots</div>;
+const NavigationDots = ({ active }) => {
+  return (
+    <div className='app__navigation'>
+      {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map((item) => (
+        // eslint-disable-next-line jsx-a11y/anchor-has-content
+        <a
+          className='app__navigation-dot'
+          href={`#${item}`}
+          key={item}
+          style={active === item ? { backgroundColor: '#313BAC' } : {}}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default NavigationDots;

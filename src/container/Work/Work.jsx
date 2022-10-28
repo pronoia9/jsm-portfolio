9538163ac4dc63769169807b08caa5dd46f9b79f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-28 17:50:41 +0300

    rename style classes

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -30,10 +30,10 @@ const Work = () => {
         My creative <span>portfolio</span> section
       </h2>
 
-      <div className='app__work-filter'>
+      <div className='app__works-filter'>
         {categories.map((item, i) => (
           <div
-            className={`app__work-filter-item app__flex p-text${activeFilter === item ? ' item-active' : ''}`}
+            className={`app__works-filter-item app__flex p-text${activeFilter === item ? ' item-active' : ''}`}
             key={i}
             onClick={() => handleWorkFilter(item)}>
             {item}
@@ -42,16 +42,16 @@ const Work = () => {
       </div>
 
       <motion.div
-        className='app__work-portfolio'
+        className='app__works-portfolio'
         animate={animateCard}
         transition={{ duration: 0.5, delayChildren: 0.5 }}>
         {filterWork.map((work, i) => (
-          <div className='app__work-item app__flex' key={i}>
-            <div className='app__work-img app__flex'>
+          <div className='app__works-item app__flex' key={i}>
+            <div className='app__works-img app__flex'>
               <img src={urlFor(work.imgUrl)} alt={work.name} />
 
               <motion.div
-                className='app__work-hover app__flex'
+                className='app__works-hover app__flex'
                 whileHover={{ opacity: [0, 1] }}
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0 }}>
                 <a href={work.projectLink} target='_blank' rel='noreferrer'>
@@ -76,12 +76,12 @@ const Work = () => {
               </motion.div>
             </div>
 
-            <div className='app__work-content app__flex'>
+            <div className='app__works-content app__flex'>
               <h4 className='bold-text'>{work.title}</h4>
               <p className='p-text' style={{ marginTop: 10 }}>
                 {work.description}
               </p>
-              <div className='app__work-tag app__flex'>
+              <div className='app__works-tag app__flex'>
                 <p className='p-text'>{work.tags[0]}</p>
               </div>
             </div>

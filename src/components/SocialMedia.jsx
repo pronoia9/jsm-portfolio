eee7279103940e98d423a7d5d836a33a109983e1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-28 16:25:25 +0300

    format

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,20 +1,18 @@
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
-const SocialMedia = () => {
-  return (
-    <div className='app__social'>
-      <div>
-        <BsTwitter />
-      </div>
-      <div>
-        <FaFacebookF />
-      </div>
-      <div>
-        <BsInstagram />
-      </div>
+const SocialMedia = () => (
+  <div className='app__social'>
+    <div>
+      <BsTwitter />
     </div>
-  );
-};
+    <div>
+      <FaFacebookF />
+    </div>
+    <div>
+      <BsInstagram />
+    </div>
+  </div>
+);
 
 export default SocialMedia;

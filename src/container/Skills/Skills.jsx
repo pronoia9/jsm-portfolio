774a4be11547d170d6ff55702cf98c20a7ee1423
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-29 15:11:51 +0300

    add a check for experiences

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -37,34 +37,38 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
-        <div className='app__skills-exp'>
-          {experiences?.sort((a, b) => b.year - a.year).map((experience) => (
-              <motion.div className='app__skills-exp-item' key={experience.year}>
-                <div className='app__skills-exp-year'>
-                  <p className='bold-text'>{experience.year}</p>
-                </div>
-                <motion.div className='app__skills-exp-works'>
-                  {experience?.works?.map((work) => (
-                    <>
-                      <motion.div
-                        className='app__skills-exp-work'
-                        whileInView={{ opacity: [0, 1] }}
-                        transition={{ duration: 0.5 }}
-                        data-tip
-                        data-for={work.name}
-                        key={work.name}>
-                        <h4 className='bold-text'>{work.name}</h4>
-                        <p className='p-text'>{work.company}</p>
-                      </motion.div>
-                      <ReactTooltip id={work.name} className='skills-tooltip' effect='solid' arrowColor='#fff'>
-                        {work.desc}
-                      </ReactTooltip>
-                    </>
-                  ))}
+        {experiences.length > 0 && (
+          <div className='app__skills-exp'>
+            {experiences
+              ?.sort((a, b) => b.year - a.year)
+              .map((experience) => (
+                <motion.div className='app__skills-exp-item' key={experience.year}>
+                  <div className='app__skills-exp-year'>
+                    <p className='bold-text'>{experience.year}</p>
+                  </div>
+                  <motion.div className='app__skills-exp-works'>
+                    {experience?.works?.map((work) => (
+                      <>
+                        <motion.div
+                          className='app__skills-exp-work'
+                          whileInView={{ opacity: [0, 1] }}
+                          transition={{ duration: 0.5 }}
+                          data-tip
+                          data-for={work.name}
+                          key={work.name}>
+                          <h4 className='bold-text'>{work.name}</h4>
+                          <p className='p-text'>{work.company}</p>
+                        </motion.div>
+                        <ReactTooltip id={work.name} className='skills-tooltip' effect='solid' arrowColor='#fff'>
+                          {work.desc}
+                        </ReactTooltip>
+                      </>
+                    ))}
+                  </motion.div>
                 </motion.div>
-              </motion.div>
-            ))}
-        </div>
+              ))}
+          </div>
+        )}
       </div>
     </>
   );

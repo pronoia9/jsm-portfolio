467cdf95247f5223c4ad442232749a6677cc48fe
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-29 15:32:50 +0300

    fix image url

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -9,16 +9,24 @@ import './Testimonial.scss';
 const Testimonial = () => {
   const [brands, setBrands] = useState([]);
   const [testimonials, setTestimonials] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState();
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     client.fetch('*[_type == "brands"]').then((data) => { setBrands(data); });
-    client.fetch('*[_type == "testimonials"]').then((data) => {setTestimonials(data); });
+    client.fetch('*[_type == "testimonials"]').then((data) => { setTestimonials(data); });
   }, []);
 
   const handleClick = (index) => { setCurrentIndex(index); };
 
-  return <div className='app__testimonial-item app__flex'></div>;
+  return (
+    <>
+      {testimonials.length && (
+        <div className='app__testimonial-item app__flex'>
+          <img src={urlFor(testimonials[currentIndex].imgurl)} alt='testimonial' />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default AppWrap(MotionWrap(Testimonial, 'app__testimonial'), 'testimonial', 'app__primarybg');

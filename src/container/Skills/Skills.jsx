dc5a15b17db061b7f442068b929510b6a8e785a0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-29 13:59:09 +0300

    fetch experiences and skills

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -6,8 +6,27 @@ import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Skills.scss';
 
-const Skills = () => <>
-  <h2 className='head-text'>Skills & Experience</h2>
-</>;
+const Skills = () => {
+  const [experiences, setExperiences] = useState([]);
+  const [skills, setSkills] = useState([]);
+  useEffect(() => {
+    client.fetch('*[_type == "experiences"]').then((data) => { setExperiences(data); });
+    client.fetch('*[_type == "skills"]').then((data) => { setSkills(data); });
+  }, [])
+
+  return (
+    <>
+      <h2 className='head-text'>Skills & Experience</h2>
+
+      <div className='app__skills-container'>
+        <motion.div className='app__skills-list'>
+          {skills.map((skill, i) => (
+            <></>
+          ))}
+        </motion.div>
+      </div>
+    </>
+  );
+};
 
 export default AppWrap(MotionWrap(Skills, 'app__skills'), 'skills', 'app__whitebg');

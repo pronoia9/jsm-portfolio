a03a098b520a02252e78687b052c811942c10767
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-29 13:46:24 +0300

    add heading

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -6,6 +6,8 @@ import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Skills.scss';
 
-const Skills = () => <></>;
+const Skills = () => <>
+  <h2 className='head-text'>Skills & Experience</h2>
+</>;
 
 export default AppWrap(MotionWrap(Skills, 'app__skills'), 'skills', 'app__whitebg');

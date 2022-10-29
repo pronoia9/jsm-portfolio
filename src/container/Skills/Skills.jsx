5c21fcd580e2424880507ee8b82f3378cb172cf6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-29 13:38:40 +0300

    init skills

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -1,7 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import ReactTooltip from 'react-tooltip';
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Skills.scss';
 
 const Skills = () => {
   return <div>Skills</div>;
 };
 
-export default Skills;
+export default AppWrap(MotionWrap(Skills, 'app__skills'), 'skills', 'app__whitebg');

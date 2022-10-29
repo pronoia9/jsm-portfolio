ad8f76fe48c8b057443baafb2124a65179cac376
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-29 15:17:51 +0300

    fetch brands and testimonials

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -7,7 +7,20 @@ import { urlFor, client } from '../../client';
 import './Testimonial.scss';
 
 const Testimonial = () => {
-  return <div className=''></div>;
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState();
+
+  useEffect(() => {
+    client.fetch('*[_type == "brands"]').then((data) => { setBrands(data); });
+    client.fetch('*[_type == "testimonials"]').then((data) => {setTestimonials(data); });
+  }, []);
+
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  };
+
+  return <div className='app__testimonial-item app__flex'></div>;
 };
 
 export default AppWrap(MotionWrap(Testimonial, 'app__testimonial'), 'testimonial', 'app__primarybg');

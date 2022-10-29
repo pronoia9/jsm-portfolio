8eeccb666a9a8998b6079390b14d3c8f91c956fe
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-29 15:14:24 +0300

    init testimonial

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -1,7 +1,13 @@
+import { useState, useEffect } from 'react';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Testimonial.scss';
 
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  return <div className=''></div>;
 };
 
-export default Testimonial;
+export default AppWrap(MotionWrap(Testimonial, 'app__testimonial'), 'testimonial', 'app__primarybg');

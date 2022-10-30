20f0a37a050b77166c773c6662063f1d42ab7200
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-30 19:08:38 +0300

    add name input + states

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -6,6 +6,10 @@ import { client } from '../../client';
 import './Footer.scss';
 
 const Footer = () => {
+  const [name, setName] = useState('');
+
+  const handleChangeInput = () => {};
+
   return (
     <>
       <h2 className='head-text'>Take a coffee & chat with me</h2>
@@ -21,6 +25,19 @@ const Footer = () => {
           <a className='p-text' href='tel:+1 123 456 789'>
             +1 123 456 789
           </a>
+        </div>
+      </div>
+
+      <div className='app__footer-form app__flex'>
+        <div className='app__flex'>
+          <input
+            className='p-text'
+            type='text'
+            name=''
+            placeholder='Your Name'
+            value={name}
+            onChange={() => handleChangeInput()}
+          />
         </div>
       </div>
     </>

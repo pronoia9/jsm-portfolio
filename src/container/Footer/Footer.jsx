b04524bc8c359aaf64a70abc69515133c818e80f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-30 19:04:57 +0300

    add image + email

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -8,6 +8,12 @@ import './Footer.scss';
 const Footer = () => {
   return <>
     <h2 className='head-text'>Take a coffee & chat with me</h2>
+    <div className='app__footer-cards'>
+      <div className='app__footer-card'>
+        <img src={images.email} alt='email' />
+        <a className='p-text' href='mailto:jayansin9@gmail.com'>jayansin9@gmail</a>
+      </div>
+    </div>
   </>;
 };
 

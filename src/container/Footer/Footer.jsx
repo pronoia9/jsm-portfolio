9e72a6c8a752753f08c3a97cbd22d5a946f28c9e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-30 19:01:57 +0300

    init footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,7 +1,12 @@
+import React, { useState } from 'react';
+
+import { images } from '../../constants';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { client } from '../../client';
 import './Footer.scss';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div className=''></div>;
 };
 
-export default Footer;
+export default AppWrap(MotionWrap(Footer, 'app__footer'), 'contact', 'app__whitebg');

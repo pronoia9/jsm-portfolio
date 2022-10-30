f22e63a19e921955b84172dbef4479ffbc344b61
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-30 19:06:36 +0300

    add phone number

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -6,15 +6,25 @@ import { client } from '../../client';
 import './Footer.scss';
 
 const Footer = () => {
-  return <>
-    <h2 className='head-text'>Take a coffee & chat with me</h2>
-    <div className='app__footer-cards'>
-      <div className='app__footer-card'>
-        <img src={images.email} alt='email' />
-        <a className='p-text' href='mailto:jayansin9@gmail.com'>jayansin9@gmail</a>
+  return (
+    <>
+      <h2 className='head-text'>Take a coffee & chat with me</h2>
+      <div className='app__footer-cards'>
+        <div className='app__footer-card'>
+          <img src={images.email} alt='email' />
+          <a className='p-text' href='mailto:jayansin9@gmail.com'>
+            jayansin9@gmail
+          </a>
+        </div>
+        <div className='app__footer-card'>
+          <img src={images.mobile} alt='mobile' />
+          <a className='p-text' href='tel:+1 123 456 789'>
+            +1 123 456 789
+          </a>
+        </div>
       </div>
-    </div>
-  </>;
+    </>
+  );
 };
 
 export default AppWrap(MotionWrap(Footer, 'app__footer'), 'contact', 'app__whitebg');
